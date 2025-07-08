85a16e4a1d643864d59d6457f487cd058e3eb630
unknown <gdutt2304@gmail.com>
2025-07-08 12:20:50 +0530

    Update basic

--- js/code1.js
+++ js/code1.js
@@ -3,7 +3,46 @@
 
 // console.log(a+b)
 
-console.log("Ganesh Dutt")
+// console.log("Ganesh Dutt")
+
+
+// a = "Ganesh "
+// console.log(a);
+
+//  var str  = "Ganesh Dutt"
+// console.log(str);
+
+// var str = 34
+// str = 78
+// var str = "hello "
+
+// console.log(str);
+
+// let value1 = 45;
+// value1 = 89
+// console.log(value1);
+
+// let value1 = 56;
+
+
+// const constValue1 = 10;
+// constValue1= 67;
+// console.log(constValue1);
+
+// const constValue1 = 45;
+
+
+// 1k
+
+
+let str  = prompt("Enter your name ")
+// alert(str)
+
+console.log("Hey Good Morning " + str );
+
+
+
+
 
 
 // comments  = ctrl + /
