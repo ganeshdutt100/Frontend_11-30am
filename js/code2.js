1e549b0984750661be9e6c68893e0373478ccf1b
unknown <gdutt2304@gmail.com>
2025-07-09 12:23:24 +0530

    Update js code2

---
+++ js/code2.js
@@ -0,0 +1,69 @@
+// let a = "45"
+// console.log( typeof a);
+
+// let value  = 45;
+
+
+// 56   -  78
+
+// let a = 10;
+
+// let b = 2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// a++;
+// a--
+// a-1 = 9
+// a++;
+// console.log(a++); //10
+// console.log(a);// 11/
+
+
+// a++ 
+// a--
+// console.log(--a);
+
+// console.log(a--); 
+
+
+// let a = "56";
+// let b  = 56;
+
+// console.log(a === b);
+// console.log(a == b);
+// console.log(a > b);
+// console.log(a < b);
+
+// console.log(a>=b);
+// console.log(a<=b);
+
+// console.log(!(a == b));
+
+
+
+// OR(||)  , AND(&&) , NOT(!)
+
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+
+// true || true = true
+// true || false = true
+// false || false = false
+
+// !(true) = false 
+
+// let a = 7;
+ //a = a * 5; 
+//   a *=5
+// console.log(a);
+// let a = 89;
+
+// let  output  = (a % 2 == 0)? "even":"odd"
+// console.log(output);
+
