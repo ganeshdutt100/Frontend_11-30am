0b93e9cc48c5e20595108274c476199c351a848c
unknown <gdutt2304@gmail.com>
2025-07-10 12:25:00 +0530

    Update If else

---
+++ js/code3.js
@@ -0,0 +1,42 @@
+// let a = 57;
+
+
+// if(a % 2 == 0){
+//     console.log("Even number"); // true 
+// } 
+
+// if(condition){
+//     stm
+// }
+
+// if(a % 2 == 0){
+//     console.log("Even number"); // true 
+// } else{
+//     console.log("Odd number"); // false
+// }
+
+
+
+// if(condition1){
+//     stm
+// }else if(condition2){
+//     stm
+// }else if(condition3){
+// stm
+// }else{
+//     stm
+// }
+
+
+let marks = 40;
+
+if(marks >= 80){
+    console.log("A grade");
+}
+ else if(marks >= 60){
+    console.log("B grade");
+} else if(marks >= 40){
+    console.log("C grade");
+} else{
+    console.log("D grade");
+}
