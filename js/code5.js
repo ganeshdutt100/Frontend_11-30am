bbc40c44aa440c5e884be8a2b3e3a986b49a592e
unknown <gdutt2304@gmail.com>
2025-07-14 12:10:35 +0530

    Update login

---
+++ js/code5.js
@@ -0,0 +1,17 @@
+let correctEmail = "trainerganeshdutt@gmail.com"
+let correctPassword = "123456789"
+
+let email = "trainerganeshdutt@gmail.com"
+let password = "123456789"
+
+if(email ==  correctEmail){
+ if(password ==  correctPassword){
+    console.log("Login Successful");
+    
+ }else{
+    console.log("Wrong Password");
+ }
+}else{
+    console.log("Invalid Email ");
+    
+}
