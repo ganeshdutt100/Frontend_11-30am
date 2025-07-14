431fe716db7aa9bfb74a72f168fe26d1343421f0
unknown <gdutt2304@gmail.com>
2025-07-14 13:03:18 +0530

    Loop

---
+++ js/code6.js
@@ -0,0 +1,43 @@
+// let  a = 1;
+// console.log(a);
+
+// for(let i = 1; i <= 100; i++){
+//     console.log( i +"  😂");
+    
+// }
+
+// let i = 2;
+// while(true){
+//     console.log(i);
+//     i++;
+//     if(i==5) break;
+    
+// }
+
+
+//  let  i = 16;
+//  do{
+//     console.log(i);
+//     i++
+    
+//  }while(i <=10)
+
+let arr = [2,6789,76,543]
+
+for(let x of arr ){
+    console.log(x);
+    
+}
+
+
+// let person  =  {
+//    name:"Ganesh dutt",
+//    age:25,
+//    city:"Mathura",
+//    state:"UP",
+//    country:"India",
+// }
+// console.log(person.state , person.name);
+// for(let value  in person){
+//     console.log(value ,  person[value]);
+// }
