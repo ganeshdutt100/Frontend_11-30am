e4acbdab1f4e88bf88eb11f49902a4e842c670cd
unknown <gdutt2304@gmail.com>
2025-07-14 12:28:02 +0530

    Update questin - 4

--- js/code5.js
+++ js/code5.js
@@ -1,17 +1,32 @@
-let correctEmail = "trainerganeshdutt@gmail.com"
-let correctPassword = "123456789"
+// let correctEmail = "trainerganeshdutt@gmail.com"
+// let correctPassword = "123456789"
 
-let email = prompt("Enter your email ")
-let password = prompt("Enter your password ")
+// let email = prompt("Enter your email ")
+// let password = prompt("Enter your password ")
 
-if(email ==  correctEmail){
- if(password ==  correctPassword){
-    console.log("Login Successful");
+// if(email ==  correctEmail){
+//  if(password ==  correctPassword){
+//     console.log("Login Successful");
     
- }else{
-    console.log("Wrong Password");
+//  }else{
+//     console.log("Wrong Password");
+//  }
+// }else{
+//     console.log("Invalid Email ");
+    
+// }
+
+
+let char = prompt("Enter your Value ")
+
+if(char.length == 1 && char  >= 'a' && char <= 'z'){
+ if(char == 'a' || char == 'e' || char == "i" || char == "o" || char == "u"){
+    console.log("It is a vowel");
+ }
+ else{
+    console.log("It is a consonant");
  }
 }else{
-    console.log("Invalid Email ");
+    console.log("Invalid Input" );
     
 }
