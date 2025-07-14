1a5015a324a653736c89e059486beb710264055e
unknown <gdutt2304@gmail.com>
2025-07-14 11:54:29 +0530

    Switch

---
+++ js/code4.js
@@ -0,0 +1,20 @@
+let a = 1;
+
+switch(a){
+    case 1:
+        console.log('a is 1');
+        break;
+    case 2:
+        console.log('a is 2');
+        break;
+    case 3:
+        console.log('a is 3');
+        break;
+    case 4:
+        console.log('a is 4');
+        break;
+
+        default:
+        console.log('a is not 1 or 2 or 3 or 4');
+        
+}
