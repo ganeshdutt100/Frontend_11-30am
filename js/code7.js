2769b7e97091c3bd717be3b3b1d1d713c82e334d
unknown <gdutt2304@gmail.com>
2025-07-15 13:27:41 +0530

    Questions

---
+++ js/code7.js
@@ -0,0 +1,178 @@
+// Quesitons-1
+//2*1=2
+//2*2=4
+//2*3=6
+// .
+// let table = 34
+// for(let i = 1; i <=10; i++){
+
+// console.log( table + " * " + i + " = " + i*table);
+
+// }
+
+// let value  = prompt("Enter your number ")
+// console.log(value);
+
+// question-2
+// Print Characters of a String  = > "Ganesh"
+
+//   let str = "Ganesh"
+//   for(let char  of str){
+//     console.log(char);
+    
+//   }  
+
+// question-3
+// Print numbers from 10 to 1 (reverse order)
+//  for(let i = 10; i>=1; i-- ){
+//     console.log(i);
+    
+//  }
+ 
+// question - 4
+//  Find factorial of a number
+//  5  -> 5*4*3*2*1= 120 
+// 1*2*3*4*5 = 120
+//  let num = 8
+//  let fact =1 ;
+//  for(let i = 1 ; i<= num; i++){
+//     fact *=i;
+//  }
+
+//  console.log(fact);
+ 
+
+// question - 5
+// Sum of numbers from 1 to 100
+// let sum = 0;
+// for(let i = 1 ; i<= 100; i++){
+//      sum= sum+i
+
+// }
+//     console.log(sum);
+
+
+
+    // 45 + 0 = 45
+    // 45  *  0 = 0
+    // 45+1 = 46
+    // 45*1 = 45
+
+    // Reverse a string using a loop
+    // "Ganesh"
+
+    // let str  = "Ganesh";
+    // let result  = "";
+    // for(let i = str.length -1; i >=0; i--){
+    //      result += str[i];
+    // }
+    // console.log(result);
+    
+    
+
+    // 4. Print all unique elements from an array using nested loops (no Set):
+
+   
+    // -- > const arr = [1, 2, 3, 2, 4, 1, 5];
+//  3,4,5
+
+// 1
+//  let x = 5;
+// if (x > 3 && x < 10) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+// A. True   -
+// B. False
+// C. Error
+// D. undefined
+
+//2 Which operator is used to assign a value?
+// A. ==
+// B. ===
+// C. =   -
+// D. :=
+
+// 3- for (let i = 0; i < 3; i++) {
+//   console.log(i);
+// }
+// A. 1 2 3
+// B. 0 1 2 -
+// C. 0 1 2 3
+// D. Infinite Loop
+
+
+// 4 What is the result of 5 + "5" in JavaScript?
+// A. 10
+// B. "55" -
+// C. NaN
+// D. Error
+
+
+//5 let num = 8;
+// if (num % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// A. Even -
+// B. Odd
+// C. undefined
+// D. Error
+
+// 6  let a = 10;
+// let b = 20;
+// console.log(a > b ? "A is greater" : "B is greater");
+// A. A is greater
+// B. B is greater -
+// C. Error
+// D. undefined
+
+// 7. Which of the following is a valid for loop syntax?
+// A. for let i = 0; i < 5; i++
+// B. for (i = 0; i < 5; i++) -
+// C. for (i <= 5; i++)
+// D. loop (i = 0; i < 5; i++)
+
+// 8 let marks = 85;
+// if (marks >= 80) {
+//   if (marks >= 90) {
+//     console.log("Grade A");
+//   } else {
+//     console.log("Grade B");
+//   }
+// }
+
+// A. Grade A
+// B. Grade B -
+// C. Grade C
+// D. Nothing
+
+
+// 9. Which loop guarantees to run at least once?
+// A. for
+// B. while
+// C. do...while -
+// D. forEach
+
+
+
+
+// let i = 1;
+// while (i < 3) {
+//   console.log(i);
+//   i++;
+// }
+// A. 0 1 2
+// B. 1 2 -
+// C. 1 2 3
+// D. Infinite loop
+
+
+
+// let a = 5
+// let b = "5"
+// let c = a+ b
+// console.log(typeof c);
