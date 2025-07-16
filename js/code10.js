541977948030041ed7105cc676366deb296d5215
unknown <gdutt2304@gmail.com>
2025-07-16 12:30:38 +0530

    Update Functions

---
+++ js/code10.js
@@ -0,0 +1,48 @@
+// function helloWorld(){
+//     console.log("Hello Khushi");
+    
+// }
+
+// helloWorld();
+
+
+// function add(a ,b){
+//     return console.log(a + b);
+    
+// }
+// add(10,20);
+
+// let fun = function(){
+//     console.log("Hello World");
+// }
+
+// let fun = function(a,b,c){
+//   return  console.log(a+b*c);
+// }
+
+// fun(2,2,4);
+
+// (function(){
+//     console.log("Hello World1");
+// })();
+// (function(){
+//     console.log("Hello World 2");
+// })();
+// (function(){
+//     console.log("Hello World 3");
+// })();
+// (function(){
+//     console.log("Hello World 4");
+// })();
+    
+(function(){
+    console.log("Hello World 5");
+})()
+let arrowFunction =  () => console.log("Hello World");
+let arrowFunction1 =  () =>{
+     console.log("Hello World");
+     console.log("Hello World");
+     console.log("Hello World");
+     console.log("Hello World");
+}
+arrowFunction1()
