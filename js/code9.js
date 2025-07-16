6afc19bc3cce2c15607052626d10d789af005a53
unknown <gdutt2304@gmail.com>
2025-07-16 12:08:11 +0530

    Array

---
+++ js/code9.js
@@ -0,0 +1,41 @@
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// let e = 50;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// let arr = [10,20,30,40,50 ,"String", true, false, null, undefined];
+
+let arr = [34,56,78,654,32,2,1,5,6,7,8,9,10];
+
+let sum = 0;
+ for(let x in arr){
+    // console.log(arr[x] + 2);
+    sum = sum + arr[x];
+    
+ }
+
+    console.log(sum);
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+// console.log(arr[10]);
+// console.log(arr[11]);
+// console.log(arr[12]);
+
+
