0d2407619399304535d3453cd8d80097ce7eee76
unknown <gdutt2304@gmail.com>
2025-07-17 12:57:24 +0530

    Apply Method

--- js/code11.js
+++ js/code11.js
@@ -7,23 +7,33 @@ let name  = "Ganesh Dutt"
 // }
 // fun.call(null ,"Vaibhav" );
 
-function studentInfo(name , course , id ){
-     this.name = name
-     this.course = course
-     this.id = id
-}
-function Student(name, course , id ){
-    studentInfo.call(this,name,course,id);
-}
-function tempStudent(name, course , id , Salary  ){
-    this.Salary = Salary
-    studentInfo.call(this,name,course,id);
-}
+// function studentInfo(name , course , id ){
+//      this.name = name
+//      this.course = course
+//      this.id = id
+// }
+// function Student(name, course , id ){
+//     studentInfo.call(this,name,course,id);
+// }
+// function tempStudent(name, course , id , Salary  ){
+//     this.Salary = Salary
+//     studentInfo.call(this,name,course,id);
+// }
  
-let Kartik = new Student("Kartik" , "Frontend" , 101);
-let Vaibhav = new Student("Vaibhav" , "Frontend" , 103);
-let Khushi = new tempStudent("Khushi" , "fs" , 102,123456789);
-console.log(Kartik  , Vaibhav);
-console.log(Khushi);
+// let Kartik = new Student("Kartik" , "Frontend" , 101);
+// let Vaibhav = new Student("Vaibhav" , "Frontend" , 103);
+// let Khushi = new tempStudent("Khushi" , "fs" , 102,123456789);
+// console.log(Kartik  , Vaibhav);
+// console.log(Khushi);
 // console.log(Student2);
+
+
+// Apply method
+// let arr =[12,45,8,34,5]
+// let max =  Math.max.apply(this,arr);
+// console.log(max);
+
+// let min =  Math.min.apply(this,arr);
+// console.log(min);
+
 
