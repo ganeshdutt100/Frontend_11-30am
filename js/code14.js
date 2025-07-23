7abc80a2f81c217ad64102117370d7717ef6cfef
unknown <gdutt2304@gmail.com>
2025-07-23 12:54:05 +0530

    Update rock , paper, scissor

--- js/code14.js
+++ js/code14.js
@@ -1,27 +1,47 @@
-function fun() {
-  let arr = ["rock", "paper", "scissors"];
+
+
+let userScore = 0
+let computerScore = 0
+
+function fun(user) {
+let output  =  document.getElementById('output')
+
+
+  let arr = ["rock", "paper", "scissor"];
   let computerInput = arr[Math.floor(Math.random() * 3)];
   //   console.log(computerInput);
 
-  let user = "paper";
+  
 
   if (user == computerInput) {
-    console.log("Tie this Game!");
+    // console.log("Tie this Game!");
+    output.innerHTML="Tie this Game!"
   } else if (
-    (user == "rock" && computerInput == "scissors") ||
-    (user == "scissors" && computerInput == "paper") ||
+    (user == "rock" && computerInput == "scissor") ||
+    (user == "scissor" && computerInput == "paper") ||
     (user == "paper" && computerInput == "rock")
   ) {
-    console.log(
-      `user win , user choice ${user} and computer choice ${computerInput}`
-    );
+    // console.log(
+    //   `user win , user choice ${user} and computer choice ${computerInput}`
+    // );
+    output.innerHTML=`user win , user choice ${user} and computer choice ${computerInput}`
+    userScore++;
   } else {
-    console.log(
-      `computer win , computer choice ${computerInput} and user choice ${user}`
-    );
+    // console.log(
+    //   `computer win , computer choice ${computerInput} and user choice ${user}`
+    // );
+    output.innerHTML= `computer win , computer choice ${computerInput} and user choice ${user}`
+    computerScore++;
   }
+  score();
 }
-fun();
+
+function score(){
+    document.getElementById('userScore').innerHTML = `computer score ${computerScore}`
+    document.getElementById('computerScore').innerHTML=`user score ${userScore}`
+
+}
+// fun();
 //   user
 //   r  = s
 //   s  = p
