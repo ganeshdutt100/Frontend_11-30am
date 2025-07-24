eddfd78e17e30b3078bdfd7968e50feab8d6724d
unknown <gdutt2304@gmail.com>
2025-07-24 13:17:40 +0530

    Array Constructor:

---
+++ js/code16.js
@@ -0,0 +1,16 @@
+// let emptyArr =[];
+// emptyArr[0]="Ganesh"
+// emptyArr[1]="Dutt"
+// console.log(emptyArr);
+
+// let newArray = new Array();
+// newArray[0]="Khushi"
+// newArray[1]="Vaibhav"
+// newArray[2]="Kartik"
+// newArray[3]="Sarthak"
+// newArray[4]="Kanav"
+// newArray[5]="Aqsa"
+
+
+// let newArray = new Array("Khushi" , "Vaibhav" , "Kartik" , "Sarthak" , "Kanav" , "Aqsa");
+// console.log(newArray);
