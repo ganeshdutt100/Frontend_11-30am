53b9b394d605580092a6df84f5f7400964a47a96
unknown <gdutt2304@gmail.com>
2025-07-28 12:29:03 +0530

    Update Array Methods(Mutator)

---
+++ js/code17.js
@@ -0,0 +1,20 @@
+let arr =[1,2,5,8,12,34,56,78,39]
+// 1 fill - (value  , start , end (n-1))
+// let output  = arr.fill("JS",2, 5);
+// 2 pop()
+// let output  = arr.pop();
+// 3 push()
+// let output  = arr.push("Ganesh Dutt");
+// 4 reverse()
+// let output  = arr.reverse();
+// 5 shift()
+// let output  = arr.shift();
+// 6 unshift()
+// let output  = arr.unshift("Ganesh Dutt");
+// 7 sort()
+// let output  = arr.sort(); // 0-9 and 10- 99 || 100 - 999
+// 8 splice(start, deleteCount, Item1, Item2,....)
+// let output  = arr.splice(1,0,"js","css","TailwindCSS","HTML"); 
+
+// console.log(output);
+console.log(arr);
