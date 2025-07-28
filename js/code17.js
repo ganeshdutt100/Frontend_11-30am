a14e7a29e295f28c3c2f6466bb0badeacd14d795
unknown <gdutt2304@gmail.com>
2025-07-28 13:01:59 +0530

    Update Array Methods(Accessor)

--- js/code17.js
+++ js/code17.js
@@ -1,4 +1,20 @@
-let arr =[1,2,5,8,12,34,56,78,39]
+// let arr =[1,2,5,8,12,34,56,78,39]
+// copywithin(target, start ,end)
+// arr.copyWithin(0,3,5);
+
+
+//  let arr = ['apple','banana', 'orange','mango','kiwi','grape','js'];
+//  arr.copyWithin(4,0,5)
+
+ // target  - orange 
+ // starting  - apple
+ //end  - banana
+ //'apple','banana', 'orange'
+
+ //apple , banana , apple ,banana, kiwi, grape
+ //apple , banana,'apple','banana', 'orange' 
+
+
 // 1 fill - (value  , start , end (n-1))
 // let output  = arr.fill("JS",2, 5);
 // 2 pop()
@@ -17,4 +33,28 @@ let arr =[1,2,5,8,12,34,56,78,39]
 // let output  = arr.splice(1,0,"js","css","TailwindCSS","HTML"); 
 
 // console.log(output);
-console.log(arr);
+// console.log(arr);
+
+
+
+// 2 - Accessor 
+let arr1 = [22,1,2,34,5,6,22,7]
+// let arr2 = [434,5,6,7,65,4,56,54]
+
+// 0Concat()
+// let output  = arr1.concat(arr2)
+// console.log(output);
+
+// 1 Include 
+// let output  = arr1.includes(22)
+// 2 IndexOf 
+// let output  = arr1.indexOf(22)
+// 3 join
+// let output  = arr1.join( ' / ')
+// 4 lastIndexOf
+// let output  = arr1.lastIndexOf(22)
+// 5 slice(start, end )
+// let output  = arr1.slice(1,2)
+// 6 toString(start, end )
+// let output  = arr1.toString()
+console.log(output);
