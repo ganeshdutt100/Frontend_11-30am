4472c5daf35bd55f97542af6d38e44a03803a87c
unknown <gdutt2304@gmail.com>
2025-08-04 12:40:12 +0530

    Object

--- js/code19.js
+++ js/code19.js
@@ -1,15 +1,62 @@
-let person ={
-    // key : "value",
-    name : "sachin",
-    age : 20,
-    address : "mumbai",
-    phone : 1234567890,
+// let person ={
+//     // key : "value",
+//     name : "sachin",
+//     age : 20,
+//     address : "mumbai",
+//     phone : 1234567890,
   
-}
-for(let key in person){
-    console.log(key, " :  ",person[key]);
-}
+// }
+// for(let key in person){
+//     console.log(key, " :  ",person[key]);
+// }
 // console.log("Name :", person.name);
 // console.log("Age :", person.age);
 // console.log("Address :", person.address);
 // console.log("Name :", person["name"] ,\n, "Age :", person["age"]);
+
+
+
+// Object Constructor
+// function Person(name , age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// let person1  = new Person("Khushi" , 20)
+// let person2  = new Person("Vaibhav" , 25)
+
+// for(let key in person1){
+//     console.log(key, " :  ",person1[key]);
+// }
+// for(let key in person2){
+//     console.log(key, " :  ",person2[key]);
+// }
+
+
+// class Person{
+//     constructor(name , age){
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+// let person1  = new Person("Khushi" , 20)
+// let person2  = new Person("Vaibhav" , 25)
+
+// for(let key in person1){
+//     console.log(key, " :  ",person1[key]);
+// }
+// for(let key in person2){
+//     console.log(key, " :  ",person2[key]);
+// }
+
+// function Person(name , age){
+//     return{
+//         name:name,
+//         age:age
+//     }
+// }
+// let student1 = new Person("Khushi", 20)
+
+//  for(let key in student1){
+//     console.log(key, " :  ",student1[key]);
+//  }
