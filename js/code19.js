209a0e743a11a3dba64bbbd983a8c92b6b729693
unknown <gdutt2304@gmail.com>
2025-08-04 12:27:33 +0530

    Update Object

---
+++ js/code19.js
@@ -0,0 +1,15 @@
+let person ={
+    // key : "value",
+    name : "sachin",
+    age : 20,
+    address : "mumbai",
+    phone : 1234567890,
+  
+}
+for(let key in person){
+    console.log(key, " :  ",person[key]);
+}
+// console.log("Name :", person.name);
+// console.log("Age :", person.age);
+// console.log("Address :", person.address);
+// console.log("Name :", person["name"] ,\n, "Age :", person["age"]);
