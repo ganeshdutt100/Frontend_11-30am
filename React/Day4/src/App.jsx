e1221703343d955db636eaeadbc9c111a164722a
unknown <gdutt2304@gmail.com>
2025-09-08 13:05:16 +0530

    Update Destructuring props

--- React/Day4/src/App.jsx
+++ React/Day4/src/App.jsx
@@ -1,5 +1,6 @@
 
 // import './App.css'
+import Cards from './components/Cards'
 import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
@@ -7,11 +8,19 @@ import './style.css'
 
 function App() {
 
+  let para1 = " Quo voluptas quod, magnam eligendi laborum modi id autem accusamus illo eum itaque nisi a necessitatibus dolor? Sequi minus vero iusto laboriosam."
+  let para2 = " Quo voluptas quod, magnam eligendi laborum modi id autem accusamus illo eum itaque nisi a necessitatibus dolor? Sequi minus vero iusto laboriosam."
+  let para3 = " Quo voluptas quod, magnam eligendi laborum modi id autem accusamus illo eum itaque nisi a necessitatibus dolor? Sequi minus vero iusto laboriosam."
   return (
     <>
-     <Navbar name="GYM"/>
-     <Navbar name="MBR"/>
+     <Navbar title="GYM"/>
      <HeroSection/>
+     <div className="cards">
+      <Cards heading="Card 1" para={para1}/>
+      <Cards heading="Card 2" para={para2}/>
+      <Cards heading="Card 3" para={para3}/>
+
+     </div>
      <Footer/>
     </>
   )
--- React/Day4/src/components/Navbar.jsx
+++ React/Day4/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = (props) => {
   return (
     <div>
         <nav className='flex justify-around bg-black text-white p-5 m-5 rounded-full'>
-      <div className="logo text-xl">{props.name}</div>
+      <div className="logo text-xl">{props.title}</div>
       <ul className='flex gap-4'>
         <li className='hover:text-red-500'><a href="#">Home</a></li>
         <li className='hover:text-red-500'><a href="#">About</a></li>
